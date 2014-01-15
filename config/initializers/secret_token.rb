00731ea9de2f824d4e5b79fc940b29500c412a07
Nicollette <nicolletteml@gmail.com>
2014-01-15 09:30:36 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodgawkerApp::Application.config.secret_token = 'c050cac021fd911031ba389ad341959f7a718e2f23ab5fd67167b222a34ee90553c7d8140a4d1bb258f938f1a398572a0692cd3e9109696a72127d41ff7c3dd2'
