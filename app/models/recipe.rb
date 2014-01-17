748c74645670df17ea1df4620f96f995c827249c
Nicollette <nicolletteml@gmail.com>
2014-01-17 11:39:19 -0800

    Added filter_by_categories class method to Recipe.rb

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -14,4 +14,35 @@ class Recipe < ActiveRecord::Base
       medium: '336x280>',
       large: '672X560>'
   }
+  
+  
+  def self.filter_by_categories(categories)
+    categories = [2, 3]
+    join_statement = categories.map do |category|
+      "SELECT
+          recipe_categories.recipe_id
+        FROM
+          recipe_categories
+        WHERE
+          recipe_categories.category_id = ?"
+    end.join("\nINTERSECT\n")
+    
+    query = <<-SQL
+      SELECT
+        recipes.*
+      FROM 
+        recipes
+      JOIN
+        (#{join_statement}) AS rc
+      ON
+        recipes.id = rc.recipe_id
+    SQL
+    sql_params = [query].concat(categories)
+    
+    results = Recipe.find_by_sql(sql_params)   
+  end
+  
+  def self.filter_by_keyword(keywords)
+    
+  end
 end
