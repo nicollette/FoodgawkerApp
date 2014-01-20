92698f69d4089e435f052dc13f58119452ee7402
Nicollette <nicolletteml@gmail.com>
2014-01-20 15:51:58 -0800

    Cleaned up comments

--- app/assets/javascripts/foodgawker_app.js
+++ app/assets/javascripts/foodgawker_app.js
@@ -19,15 +19,11 @@ window.FoodgawkerApp = {
         ["Welcome " + FoodgawkerApp.Data.currentUser.get("username")], 
         "success"
       )
-//NEED TO FIX ALERTS      
-      // var alertView = new FoodgawkerApp.Views.Alerts({ 
-      //   message: "Welcome " + FoodgawkerApp.Data.currentUser.get("username"),
-      //   classType: "alert-success"
-      // });
-      // $("#alerts-section").html(alertView.render().$el);
     }
+    
     var navView = new FoodgawkerApp.Views.NavBar();
     $("#nav-section").html(navView.render().$el);
+    
     FoodgawkerApp.Data.recipes = new FoodgawkerApp.Collections.Recipes();
     FoodgawkerApp.Data.recipes.fetch({
       data: { page: 1 },
