f80b10569865b22b0b4f9de979cb8a540a961bc1
Nicollette <nicolletteml@gmail.com>
2014-01-24 10:28:29 -0800

    Removed unnecessary new action in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
-  def new
-    render :new
-  end
+  # def new
+  #   render :new
+  # end
   
   def create
     @user = User.find_by_credentials(params[:user])
